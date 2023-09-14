cd5d728d47b6bf57f82503140d9005314421b262
psyzed <psyzed66@gmail.com>
2023-09-14 14:17:50 +0300

    added envs

--- src/app/list/list.service.ts
+++ src/app/list/list.service.ts
@@ -2,6 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Todo } from './models';
 import { BehaviorSubject, catchError, of, tap, throwError } from 'rxjs';
+import { API_URL } from 'src/environments/environment';
 
 @Injectable()
 export class ListService {
@@ -14,21 +15,19 @@ export class ListService {
   getList() {
     if (this.todos.length === 0) {
       this.isLoading.next(true);
-      return this.httpClient
-        .get<Todo[]>('https://jsonplaceholder.typicode.com/todos/')
-        .pipe(
-          tap(
-            (todos) => {
-              this.isLoading.next(false);
-              this.todos = todos;
-            },
-            catchError((error) => {
-              this.hasError.next(true);
-              this.isLoading.next(false);
-              return throwError(() => new Error('Something went wrong'));
-            })
-          )
-        );
+      return this.httpClient.get<Todo[]>(API_URL).pipe(
+        tap(
+          (todos) => {
+            this.isLoading.next(false);
+            this.todos = todos;
+          },
+          catchError((error) => {
+            this.hasError.next(true);
+            this.isLoading.next(false);
+            return throwError(() => new Error('Something went wrong'));
+          })
+        )
+      );
     } else {
       return of(this.todos);
     }
